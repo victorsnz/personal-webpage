55a5364f9d4e7f7007e857074c58c64e4657383c
Victor Sanchez <victor_sanchez@outlook.com>
2020-12-31 17:14:34 -0300

    pages update

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import '../8bits.css';
+// import '../8bits.css';
 
 import { Container, Col, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
@@ -16,13 +16,13 @@ function NotFound () {
                     <Col md={12}>
                         <section style={{textAlign:"center"}}>
                             <br/>
-                        <h1 style={{fontSize:"80px"}}>404</h1>
-                        <h1 style={{fontSize:"30px"}}>NOT FOUND</h1>
+                        <h1 id={"title-404"}>404</h1>
+                        <h1 id={"not-found"}>NOT FOUND</h1>
                         <br/>
-                        <p style={{fontSize:"15px"}}>You were wandering on strange paths, traveler.<br/>And you got lost at last...</p>
-                        <p style={{fontSize:"15px"}}>You must return safe and sound.</p>
+                        <p className={"text-404"}>You were wandering on strange paths, traveler.<br/>And you got lost at last...</p>
+                        <p className={"text-404"}>You must return safe and sound.</p>
                         <br/>
-                        <p style={{fontSize:"15px"}}>It's dangerous to go alone! take this.</p>
+                        <p className={"text-404"}>It's dangerous to go alone! take this.</p>
                         </section>
                     </Col>
                     <Col md={12}>
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import '../resume.css';
+
 import { Container, Col, Row } from 'react-bootstrap';
 
 import SideBar from '../components/Resume/SideBar';
@@ -10,32 +12,43 @@ import Certifications from '../components/Resume/Certifications';
 import Skills from '../components/Resume/Skills';
 import Interests from '../components/Resume/Interests';
 import Volunteer from '../components/Resume/Volunteer';
+import { Component } from 'react';
 
-function Resume() {
-    return(
-        <Container className="resume" fluid={true}>
-            <Row>
-            <Col id="sidebar-wrapper" md={3}>
-                <SideBar/>
-            </Col>
-            <Col md={9} id="resume-wrapper">
-                <AboutMe />
-                <hr/>
-                <Experience />
-                <hr/>
-                <Education />
-                <hr/>
-                <Certifications />
-                <hr/>
-                <Skills />
-                <hr/>
-                <Interests />
-                <hr/>
-                <Volunteer />
-            </Col>
-            </Row>
-        </Container>
-    )
+class Resume extends Component {
+
+    // openSidebar = () => {
+    //     // document.getElementById("my-sidebar").style.width = "250px";
+    //     document.getElementById("my-sidebar").style.display = "block";
+    //     console.log("funca!");
+    // }
+
+    render() {
+        return(
+            <Container className="resume" fluid={true}>
+                <Row>
+                <Col id="sidebar-wrapper" md={3} sm={12}>
+                    <SideBar/>
+                </Col>
+                <Col md={9} sm={12} id="resume-wrapper">
+                    {/* <span className={"burger-btn"} onClick={this.openSidebar}>&equiv;</span> */}
+                    <AboutMe />
+                    <hr/>
+                    <Experience />
+                    <hr/>
+                    <Education />
+                    <hr/>
+                    <Certifications />
+                    <hr/>
+                    <Skills />
+                    <hr/>
+                    <Interests />
+                    <hr/>
+                    <Volunteer />
+                </Col>
+                </Row>
+            </Container>
+        )
+    }
 }
 
 export default Resume;
