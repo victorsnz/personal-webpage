7b534ae7619018ebf4925db35d7326311a0e65b6
Victor Sanchez <victor_sanchez@outlook.com>
2021-01-19 17:23:49 -0300

    added telegram button

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,10 +9,17 @@ function Contact () {
                 <Container fluid={true}>
                     <Row> 
                         <Col md={12}>
-                            <h3 style={{textAlign: "center", color:"white", fontWeight:"300"}}>If you're looking to get in touch with me, try the button bellow.</h3>
+                            <h3 style={{textAlign: "center", color:"white", fontWeight:"300"}}>If you're looking to get in touch with me, try the buttons bellow.</h3>
                             <br/>
                             <Container fluid={true} className={"d-flex justify-content-center"}>
-                                <a href={"mailto:sanchez.v.a@gmail.com"} id={"contact-link"}>Send Email</a>
+                                <Row>
+                                    <Col  md={6} className={"d-flex justify-content-center"}>
+                                        <a href={"mailto:sanchez.v.a@gmail.com"} className={"contact-link"}>Email</a>
+                                    </Col>
+                                    <Col md={6} className={"d-flex justify-content-center"}>
+                                        <a href={"https://t.me/victorsnz"} target={"blank_"} className={"contact-link"}>Telegram</a>
+                                    </Col>
+                                </Row>
                             </Container>
                         </Col>
                     </Row>
